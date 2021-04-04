bda5ad4aa3273a58abacaaee7fb93725654390a5
Marcos Guilherme Silva <71297313+mguilherm@users.noreply.github.com>
2021-04-04 20:57:13 -0300

    Add files via upload
    Updated v2

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-function loadData() {
+function acessarAPI(url, funcao) {
   var xhttp = new XMLHttpRequest();
-
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-      mountRegion(this.responseText);
+      funcao(this.responseText);
     }
   };
-
-  xhttp.open(
-    "GET",
-    "https://servicodados.ibge.gov.br/api/v1/localidades/regioes",
-    true
-  );
+  xhttp.open("GET", url, true);
   xhttp.send();
 }
 
@@ -19,22 +13,79 @@ function mountRegion(lsRegiao) {
   var select = document.createElement("select");
   select.id = "regiao";
   select.name = "regiao";
-  select.addEventListener("change", function () {
-    console.log(this.value);
-    alert(this.value);
+  document.getElementById("regiao").addEventListener("change", function () {
+    url = "https://servicodados.ibge.gov.br/api/v1/localidades/";
+    if (document.getElementById("regiao").value == 0) {
+      url += `estados`;
+    } else {
+      url += `regioes/${this.value}/estados`;
+    }
+
+    acessarAPI(url, mountUf);
   });
 
-  document.getElementById("regioes").appendChild(select);
-  var option = document.createElement("option");
+  document.getElementById("regiao").appendChild(select);
+  var opcao = document.createElement("option");
 
   lsRegiao = JSON.parse(lsRegiao);
 
   for (i in lsRegiao) {
-    option = document.createElement("option");
-    option.value = lsRegiao[i].id;
-    option.innerHTML = lsRegiao[i].nome + " - " + lsRegiao[i].sigla;
-    document.getElementById("regioes").appendChild(option);
+    opcao = document.createElement("option");
+    opcao.value = lsRegiao[i].id;
+    opcao.innerHTML = lsRegiao[i].nome + " (" + lsRegiao[i].sigla + ")";
+    document.getElementById("regiao").appendChild(opcao);
+  }
+}
+function mountUf(lsUf) {
+  uf = document.getElementById("uf");
+  uf.innerHTML = "";
+
+  var sele = document.createElement("option");
+  sele.value = "";
+  sele.innerHTML = "Selecione";
+  document.getElementById("uf").appendChild(sele);
+
+  uf.addEventListener("change", function () {
+    acessarAPI(
+      `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${this.value}/municipios`,
+      mountMunicipios
+    );
+  });
+  lsUf = JSON.parse(lsUf);
+  for (i in lsUf) {
+    opcao = document.createElement("option");
+    opcao.value = lsUf[i].id;
+    opcao.innerHTML = lsUf[i].sigla + " - " + lsUf[i].nome;
+    uf.appendChild(opcao);
   }
 }
 
-loadData();
+function mountMunicipios(lsMunicipios) {
+  if (uf.select == [0]) {
+    municipio.innerHTML = "";
+  }
+  municipio = document.getElementById("municipio");
+  municipio.innerHTML = "";
+
+  var sele = document.createElement("option");
+  sele.value = "";
+  sele.innerHTML = "Selecione";
+  document.getElementById("municipio").appendChild(sele);
+
+  lsMunicipios = JSON.parse(lsMunicipios);
+  for (i in lsMunicipios) {
+    opcao = document.createElement("option");
+    opcao.value = lsMunicipios[i].id;
+    opcao.innerHTML = lsMunicipios[i].nome;
+    municipio.appendChild(opcao);
+  }
+}
+
+acessarAPI(
+  "https://servicodados.ibge.gov.br/api/v1/localidades/regioes",
+  mountRegion
+);
+acessarAPI(
+  "https://servicodados.ibge.gov.br/api/v1/localidades/estados",
+  mountUf
+);
